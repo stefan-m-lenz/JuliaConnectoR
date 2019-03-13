abe66343724e7eedc498701ee72888c33768f8e5
‌Stefan Lenz <stefan-m-lenz@web.de>
2019-03-13 13:10:17 +0100

    Tests use imported functions

--- tests/test.R
+++ tests/test.R
@@ -10,18 +10,16 @@ juliaCall("eval", "println(22)")
 
 juliaUsing("BoltzmannMachines", importInternal = TRUE)
 
-x <- juliaCall("barsandstripes", 100L, 4L)
+x <- barsandstripes(100L, 4L)
 x
-dbm <- juliaCall("BoltzmannMachines.fitdbm", x,
-                epochs = 40L, learningrates = c(rep(0.05, 20), rep(0.001, 20)),
-                nhiddens = c(4L,3L))
+dbm <- fitdbm(x, epochs = 40L, learningrates = c(rep(0.05, 20), rep(0.001, 20)),
+              nhiddens = c(4L,3L))
 # TODO TrainLayer arguments
-
 dbm
-juliaCall("samples", dbm, 10L)
-rbm <- juliaCall("BoltzmannMachines.fitrbm", juliaCall("barsandstripes", 100L, 4L))
-juliaCall("samples", rbm, 10L)
-juliaCall("BoltzmannMachines.Monitor")
+samples(dbm, 10L)
+rbm <- fitrbm(barsandstripes(100L, 4L))
+samples(rbm, 10L)
+BoltzmannMachines.Monitor()
 
 # Should error
 juliaCall("sum", c(1,2,3, "bla"))
