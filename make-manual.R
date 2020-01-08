4897212f0d303545111a15f6f521257d54c25df2
Stefan Lenz <lenz@imbi.uni-freiburg.de>
2020-01-08 16:53:38 +0100

    REmove tmp folder created by R CMD

--- make-manual.R
+++ make-manual.R
@@ -1,6 +1,11 @@
 library(devtools)
+# for (rdfile in list.files("man")) {
+#    file.remove(file.path("man", rdfile))
+# }
 devtools::document()
 if (file.exists("JuliaConnectoR.pdf"))  {
    file.remove("JuliaConnectoR.pdf")
 }
 system2("R", args = "CMD Rd2pdf ../JuliaConnectoR")
+subdirs <- list.dirs(recursive = FALSE)
+unlink(subdirs[grep(pattern = "\\.Rd2pdf.*", subdirs)], recursive = TRUE)
