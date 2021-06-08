d5e0011ac81356a03ac573cd4903df7796d149d4
Stefan Lenz <lenz@imbi.uni-freiburg.de>
2021-06-08 14:58:13 +0200

    Try workaround for glibc issue with alias for julia

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,13 +2,6 @@ library(testthat)
 library(JuliaConnectoR)
 library(utils)
 
-if (identical(Sys.getenv("TRAVIS"), "true")) {
-   # prevent issue with glibc that happens on Travis
-   # https://discourse.julialang.org/t/unable-to-launch-julia-1-6-ssl-and-glibc-issues/60488/5
-   Sys.unsetenv("LD_LIBRARY_PATH")
-}
-
-
 if (juliaSetupOk()) {
    test_check("JuliaConnectoR")
 }
