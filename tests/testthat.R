6c3a13856f0a7df29cd977e9a064ef9984a319c5
Stefan Lenz <lenz@imbi.uni-freiburg.de>
2021-06-10 09:12:37 +0200

    Try with empty LD_LIBRARY_PATH

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,6 +2,9 @@ library(testthat)
 library(JuliaConnectoR)
 library(utils)
 
+print("LD_LIBRARY_PATH")
+print(Sys.getenv("LD_LIBRARY_PATH"))
+
 if (juliaSetupOk()) {
    test_check("JuliaConnectoR")
 } else if (identical(Sys.getenv("TRAVIS"), "true")) {
