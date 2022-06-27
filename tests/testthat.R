bc7739791edb5686b9a1ded0fe72247ea899c354
Stefan Lenz <stefan.lenz@uni-mainz.de>
2022-06-27 16:36:41 +0200

    Renaming / clean-up

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,7 +3,6 @@ library(JuliaConnectoR)
 library(utils)
 
 if (juliaSetupOk()) {
-   cat(paste("Julia setup is OK, using Julia Version", JuliaConnectoR:::getJuliaVersionViaCmd(), "\n"))
    test_check("JuliaConnectoR")
 } else if (Sys.getenv("GITHUB_ACTIONS") == "true") {
    stop("Julia setup on GitHub Actions is not OK")
