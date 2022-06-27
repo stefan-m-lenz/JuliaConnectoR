fd3efd71dc07b644ebb7bca1ff7b7b34bfcd50d8
Stefan Lenz <stefan.lenz@uni-mainz.de>
2022-06-27 11:52:05 +0200

    Further debug GITHUB_ACTIONS env variable

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,10 +2,9 @@ library(testthat)
 library(JuliaConnectoR)
 library(utils)
 
-stop(paste0("schluss: ", Sys.getenv("GITHUB_ACTIONS")))
-
 if (juliaSetupOk()) {
    test_check("JuliaConnectoR")
-} else if (identical(Sys.getenv("GITHUB_ACTIONS"), "true")) {
+   stop("Julia setup cannot be ok")
+} else if (Sys.getenv("GITHUB_ACTIONS") == "true") {
    stop("Julia setup on GitHub Actions is not OK")
 }
